cf00a9674363f02fb46127fa01d59fac2ab0e9f7
viniciuswitczak <124414590+viniciuswitczak@users.noreply.github.com>
2025-09-28 22:21:49 -0300

    final

--- src/CifraVigenere.java
+++ src/CifraVigenere.java
@@ -1,25 +1,69 @@
+import javax.swing.JOptionPane;
+
 public class CifraVigenere {
     public static void main(String[] args) throws Exception {
-        
-        String textoOriginal = "TESTE";
-        String chave = "CHAVE";
+        String[] options = {"Cifrar", "Decifrar"};
+        int escolha = JOptionPane.showOptionDialog(
+            null,
+            "O que você deseja fazer?",
+            "Cifra de Vigenère",
+            JOptionPane.DEFAULT_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options,
+            options[0]
+        );
 
-        StringBuilder textoCifrado = new StringBuilder();
+        String texto = JOptionPane.showInputDialog(
+            "Digite o texto:"
+        );
+        String chave = JOptionPane.showInputDialog(
+            "Digite a chave:"
+        );
 
-        for (int i = 0, j = 0; i < textoOriginal.length(); i++) {
-            char letraOriginal = textoOriginal.charAt(i);
-            if (Character.isLetter(letraOriginal)) {
-                char letraChave = chave.charAt(j % chave.length());
-                char letraCifrada = (char) ((letraOriginal + letraChave - 2 * 'A') % 26 + 'A');
-                textoCifrado.append(letraCifrada);
-                j++;
-            } else {
-                textoCifrado.append(letraOriginal);
+        texto = texto.toUpperCase();
+        chave = chave.toUpperCase();
+
+        StringBuilder resultado = new StringBuilder();
+
+        if (escolha == 0) {
+            // Cifragem
+            for (int i = 0, j = 0; i < texto.length(); i++) {
+                char letraOriginal = texto.charAt(i);
+                if (Character.isLetter(letraOriginal)) {
+                    char letraChave = chave.charAt(j % chave.length());
+                    char letraCifrada = (char) ((letraOriginal + letraChave - 2 * 'A') % 26 + 'A');
+                    resultado.append(letraCifrada);
+                    j++;
+                } else {
+                    resultado.append(letraOriginal);
+                }
             }
+            JOptionPane.showMessageDialog(
+                null,
+                "Texto Original: " + texto +
+                "\nChave: " + chave +
+                "\nTexto Cifrado: " + resultado.toString()
+            );
+        } else if (escolha == 1) { 
+            // Decifragem
+            for (int i = 0, j = 0; i < texto.length(); i++) {
+                char letraCifrada = texto.charAt(i);
+                if (Character.isLetter(letraCifrada)) {
+                    char letraChave = chave.charAt(j % chave.length());
+                    char letraDecifrada = (char) ((letraCifrada - letraChave + 26) % 26 + 'A');
+                    resultado.append(letraDecifrada);
+                    j++;
+                } else {
+                    resultado.append(letraCifrada);
+                }
+            }
+            JOptionPane.showMessageDialog(
+                null,
+                "Texto Cifrado: " + texto +
+                "\nChave: " + chave +
+                "\nTexto Decifrado: " + resultado.toString()
+            );
         }
-        
-        System.out.println("Texto Original: " + textoOriginal);
-        System.out.println("Chave: " + chave);
-        System.out.println("Texto Cifrado: " + textoCifrado.toString());
     }
 }
